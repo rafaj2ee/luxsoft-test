58a8a36d82b3494a6634d81311ee57ea8baf7cb4
Rafael Nascimento Lima <feodpv01244@odontoprev.com.br>
2019-08-06 12:43:37 -0300

    Final Strings

--- src/main/java/pl/com/luxsoft/service/SearchService.java
+++ src/main/java/pl/com/luxsoft/service/SearchService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,17 @@ import pl.com.luxsoft.dto.ResultDTO;
 
 @Service
 public class SearchService {
-
+	private static final String VOLUME_INFO = "volumeInfo";
+	private static final String AUTHORS = "authors";
+	private static final String TITLE = "title";
+	private static final String KIND = "kind";
+	private static final String ITEMS = "items";
+	private static final String RESULTS = "results";
+	private static final String ARTIST_NAME = "artistName";
+	private static final String COLLECTION_NAME = "collectionName";
+	private static final String COLLECTION_TYPE = "collectionType";
+	private static final String TERM = ":term";
+	private static final String REG_EXP = "[^\\w\\d\\s]";
     @Value("${url.google}")
     private String google;
     @Value("${url.itunes}")
@@ -37,37 +48,41 @@ public class SearchService {
         //restTemplate.getForEntity(url, responseType)
         ResponseEntity<String> googleReturn = restTemplate.exchange(google, HttpMethod.GET, entity, String.class, term);
         ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, false);
+        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         try {
-			JsonNode rootNodeGoogle = mapper.readTree(googleReturn.getBody());
-			ArrayNode googleNode = (ArrayNode) rootNodeGoogle.get("items");
-			int counter1 = 0;
-			for(JsonNode node:googleNode) {
-				ResultDTO resultDTO = new ResultDTO();
-				resultDTO.setAuthor(node.findValue("volumeInfo").findValue("authors")!=null?node.findValue("volumeInfo").findValue("authors").toString().replaceAll("[^\\w\\d\\s]", ""):"");
-				resultDTO.setTitle(node.findValue("volumeInfo").findValue("title")!=null?node.findValue("volumeInfo").findValue("title").toString().replaceAll("[^\\w\\d\\s]", ""):"");
-				resultDTO.setInformation(node.findValue("kind")!=null?node.findValue("kind").toString().replaceAll("[^\\w\\d\\s]", ""):"");
-				list.add(resultDTO);
-				counter1++;
-				if(counter1==5) {
-					break;
+            if (googleReturn.getStatusCode() == HttpStatus.OK) {
+	        	JsonNode rootNodeGoogle = mapper.readTree(googleReturn.getBody());
+				ArrayNode googleNode = (ArrayNode) rootNodeGoogle.get(ITEMS);
+				int counter1 = 0;
+				for(JsonNode node:googleNode) {
+					ResultDTO resultDTO = new ResultDTO();
+					resultDTO.setAuthor(node.findValue(VOLUME_INFO).findValue(AUTHORS)!=null?node.findValue(VOLUME_INFO).findValue(AUTHORS).toString().replaceAll(REG_EXP, ""):"");
+					resultDTO.setTitle(node.findValue(VOLUME_INFO).findValue(TITLE)!=null?node.findValue(VOLUME_INFO).findValue(TITLE).toString().replaceAll(REG_EXP, ""):"");
+					resultDTO.setInformation(node.findValue(KIND)!=null?node.findValue(KIND).toString().replaceAll(REG_EXP, ""):"");
+					list.add(resultDTO);
+					counter1++;
+					if(counter1==5) {
+						break;
+					}
 				}
-			}
-	        ResponseEntity<String> itunesReturn = restTemplate.exchange(itunes.replace(":term", term), HttpMethod.GET, entity, String.class);
-			JsonNode rootNodeItunes = mapper.readTree(itunesReturn.getBody());
-			ArrayNode iTunesNode = (ArrayNode) rootNodeItunes.get("results");
-			int counter2 = 0;
-			for(JsonNode node:iTunesNode) {
-				ResultDTO resultDTO = new ResultDTO();
-				resultDTO.setAuthor(node.findValue("artistName")!=null?node.findValue("artistName").toString().replaceAll("[^\\w\\d\\s]", ""):"");
-				resultDTO.setTitle(node.findValue("collectionName")!=null?node.findValue("collectionName").toString().replaceAll("[^\\w\\d\\s]", ""):"");
-				resultDTO.setInformation(node.findValue("collectionType")!=null?node.findValue("collectionType").toString().replaceAll("[^\\w\\d\\s]", ""):"");
-				list.add(resultDTO);
-				counter2++;
-				if(counter2==5) {
-					break;
+            }
+	        ResponseEntity<String> itunesReturn = restTemplate.exchange(itunes.replace(TERM, term), HttpMethod.GET, entity, String.class);
+            if (itunesReturn.getStatusCode() == HttpStatus.OK) {
+		        JsonNode rootNodeItunes = mapper.readTree(itunesReturn.getBody());
+				ArrayNode iTunesNode = (ArrayNode) rootNodeItunes.get(RESULTS);
+				int counter2 = 0;
+				for(JsonNode node:iTunesNode) {
+					ResultDTO resultDTO = new ResultDTO();
+					resultDTO.setAuthor(node.findValue(ARTIST_NAME)!=null?node.findValue(ARTIST_NAME).toString().replaceAll(REG_EXP, ""):"");
+					resultDTO.setTitle(node.findValue(COLLECTION_NAME)!=null?node.findValue(COLLECTION_NAME).toString().replaceAll(REG_EXP, ""):"");
+					resultDTO.setInformation(node.findValue(COLLECTION_TYPE)!=null?node.findValue(COLLECTION_TYPE).toString().replaceAll(REG_EXP, ""):"");
+					list.add(resultDTO);
+					counter2++;
+					if(counter2==5) {
+						break;
+					}
 				}
-			}
+            }
 		} catch (IOException e) {
 			throw e;
 		}
